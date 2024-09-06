cc7df853270142e9a67437cfddf13fb98b379460
Ricardo Werner <wernerdive@gmail.com>
2024-09-06 16:40:21 -0300

    refactor: automatizar o carregamento do footer em todas as paginas

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,3 +1,18 @@
+// Função para carregar o rodapé
+
+export async function loadFooter(isHomePage) {
+  const footerPath = isHomePage ? "./pages/footer.html" : "./footer.html";
+
+  const response = await fetch(footerPath);
+  const footerHTML = await response.text();
+  const footerElement = document.createElement("footer");
+  footerElement.innerHTML = footerHTML;
+  document.body.appendChild(footerElement);
+
+  // Chamando a função para atualizar o ano
+  updateCurrentYear();
+}
+
 export function updateCurrentYear() {
   document.getElementById("current-year").textContent =
     new Date().getFullYear();
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,11 @@
-import { updateCurrentYear } from "./footer.js";
-// Chama a função para atualizar o ano atual
-updateCurrentYear();
-
 import carregarTextos from "./text.js";
 carregarTextos("textos.json", "tituloPagina", "textoPagina", "citacaoPagina");
+
+// Carrega o rodapé assim que a página estiver totalmente carregada
+import { loadFooter } from "./footer.js";
+
+const isHomePage = window.location.pathname === "/index.html";
+
+window.addEventListener("DOMContentLoaded", () => {
+  loadFooter(isHomePage);
+});
