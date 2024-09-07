b28980ff2bce193af68910df1e1b372613669541
Ricardo Werner <wernerdive@gmail.com>
2024-09-07 00:11:34 -0300

    feat: criado logica para renderizar dados na pagina wreck.html

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,24 @@
 import carregarTextos from "./text.js";
 carregarTextos("textos.json", "tituloPagina", "textoPagina", "citacaoPagina");
 
+import { populateGrid } from "./wreck.js";
+// Função para buscar os dados do JSON
+async function fetchData() {
+  const response = await fetch("../wreck.json");
+  const data = await response.json();
+  return data;
+}
+
+fetchData()
+  .then((data) => {
+    const gridContainer = document.querySelector(".grid-container");
+    populateGrid(data, gridContainer);
+  })
+  .catch((error) => {
+    console.error("Erro ao carregar os dados:", error);
+  });
+
+
 // Carrega o rodapé assim que a página estiver totalmente carregada
 import { loadFooter } from "./footer.js";
 
