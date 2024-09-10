0cfc3f4a10a1f8bce3ed695b4cdec0b686fe529f
Ricardo Werner <wernerdive@gmail.com>
2024-09-09 23:18:48 -0300

    refactor: correcao da logica de pesquisa e renderizacao

--- scripts/search.js
+++ scripts/search.js
@@ -1,34 +1,39 @@
 // Função para buscar os dados no arquivo JSON
-// search.js
-export async function searchData(searchTerm) {
-  const currentPage = window.location.pathname;
-  let data;
-  let propertyToSearch;
+export async function searchData(searchTerm = '') {
+    const currentPage = window.location.pathname;
+    let data;
+    let propertyToSearch;
 
-  // Definir o arquivo JSON e a propriedade de busca com base na página atual
-  if (currentPage === '/pages/dive.html') {
-    data = await fetch('../filesJSON/dive.json');  // Carrega dive.json
-    propertyToSearch = 'titleDive';   // Propriedade do dive.json
-  } else if (currentPage === '/pages/wreck.html') {
-    data = await fetch('../filesJSON/wreck.json'); // Carrega wreck.json
-    propertyToSearch = 'titleWreck'; // Propriedade do wreck.json
-  } else {
-    console.error("Página não reconhecida");
-    return [];
-  }
+    // Definir o arquivo JSON e a propriedade de busca com base na página atual
+    if (currentPage.includes('dive')) {
+        data = await fetch('../filesJSON/dive.json'); // Carrega dive.json
+        propertyToSearch = 'titleDive'; // Propriedade de busca em dive.json
+    } else if (currentPage.includes('wreck')) {
+        data = await fetch('../filesJSON/wreck.json'); // Carrega wreck.json
+        propertyToSearch = 'titleWreck'; // Propriedade de busca em wreck.json
+    } else {
+        console.error("Página não reconhecida!");
+        return [];
+    }
 
-  // Converte a resposta em JSON
-  const jsonData = await data.json();
+    // Converte a resposta em JSON
+    const jsonData = await data.json();
 
-  // Filtro de dados com base na pesquisa e na propriedade relevante para a página atual
-  const results = jsonData.filter((item) => {
-    const title = item[propertyToSearch]; // Acessa dinamicamente a propriedade
-    // Verifica se o título existe e se é uma string antes de aplicar o toLowerCase
-    return title && typeof title === 'string' && title.toLowerCase().includes(searchTerm.toLowerCase());
-  });
+    // Se o campo de pesquisa estiver vazio, retorna todos os dados
+    if (!searchTerm) {
+        return jsonData;
+    }
 
-  return results;
+    // Filtra os dados com base no termo de pesquisa
+    const results = jsonData.filter((item) => {
+        const title = item[propertyToSearch];
+        // Verifica se o título existe e se é uma string antes de aplicar o toLowerCase
+        return title && typeof title === 'string' && title.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+
+    return results;
 }
+
 
 
 
